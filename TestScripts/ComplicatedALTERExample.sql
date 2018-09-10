3b27ca75e60c4d2e2b686fca9f09f487a102aa93
Cody Konior <cody@codykonior.com>
2018-09-10 12:22:10 +0800

    BOM, TAB removal

--- TestScripts/ComplicatedALTERExample.sql
+++ TestScripts/ComplicatedALTERExample.sql
@@ -1,15 +1,15 @@
 CREATE TABLE [dbo].[SometAble](
-	[PaymentInvoiceMatchingMethodId] [int] NOT NULL,
-	[Code] [varchar](20) NOT NULL,
-	[Name] [varchar](50) NOT NULL,
-	[Description] [varchar](255) NOT NULL,
-	[CreateDate] [datetime2](7) NULL,
-	[CreateBy] [varchar](255) NULL,
-	[ModifyDate] [datetime2](7) NULL,
-	[ModifyBy] [varchar](255) NULL,
+    [PaymentInvoiceMatchingMethodId] [int] NOT NULL,
+    [Code] [varchar](20) NOT NULL,
+    [Name] [varchar](50) NOT NULL,
+    [Description] [varchar](255) NOT NULL,
+    [CreateDate] [datetime2](7) NULL,
+    [CreateBy] [varchar](255) NULL,
+    [ModifyDate] [datetime2](7) NULL,
+    [ModifyBy] [varchar](255) NULL,
 PRIMARY KEY CLUSTERED 
 (
-	[PaymentInvoiceMatchingMethodId] ASC
+    [PaymentInvoiceMatchingMethodId] ASC
 )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
--- TestScripts/IndexScript.sql
+++ TestScripts/IndexScript.sql
@@ -4,10 +4,10 @@ DROP INDEX IX_Address_AddressLine1_AddressLine2_City_StateProvinceID_PostalCode 
 
 CREATE UNIQUE NONCLUSTERED INDEX [IX_Address_AddressLine1_AddressLine2_City_StateProvinceID_PostalCode] ON [Person].[Address]
 (
-	[AddressLine1] ASC,
-	[AddressLine2] ASC,
-	[City] ASC,
-	[StateProvinceID] ASC,
-	[PostalCode] ASC
+    [AddressLine1] ASC,
+    [AddressLine2] ASC,
+    [City] ASC,
+    [StateProvinceID] ASC,
+    [PostalCode] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
